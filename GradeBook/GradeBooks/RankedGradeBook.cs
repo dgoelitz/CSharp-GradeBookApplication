f98c08674fabd7d3554e5488b2bf594939b10aa1
David Goelitz <dgoelitz@ipro.com>
2021-12-29 15:59:56 -0700

    added GetLetterGrade override to RankedGadeBook class

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,21 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int totalStudents = Students.Count;
+            double fifthOfClass = totalStudents / 5;
+            int betterThanMe = 0;
+            foreach (Student student in Students)
+            {
+                if (student.AverageGrade > averageGrade) betterThanMe++;
+            }
+            if (betterThanMe < fifthOfClass) return 'A';
+            if (betterThanMe < fifthOfClass * 2) return 'B';
+            if (betterThanMe < fifthOfClass * 3) return 'C';
+            if (betterThanMe < fifthOfClass * 4) return 'D';
+            return 'F';
+        }
     }
 }
